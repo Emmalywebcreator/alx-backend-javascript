373b8ad3525baf91111bac0a69c44e8e099fabf9
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-10-07 06:53:00 -0700

    appendstring wit for...of)

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
     const value = array[idx];
-    value.push(appendString + value);
+    value = (appendString + value);
   }
   return array;
 }
