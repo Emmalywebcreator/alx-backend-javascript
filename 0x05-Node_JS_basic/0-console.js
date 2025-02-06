d3cd23fe5e1a20835e1aadf3a95710c5cf81a01a
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2025-02-06 07:09:12 +0100

    Added author name

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,12 +1,10 @@
 /**
- * A function that display a message on STDOUT
- * @param {string} message  The message to be displayed
- * @author Emmanuel Ohwoka <https://github.com/Emmalywebcreator>
- */
-
-const displayMessage = (message) => {
-	console.log(message);
+ *  * Displays a message on the STDOUT.
+ *   * @param {String} msg The message to display.
+ *    * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ *     */
+const displayMessage = (msg) => {
+	  console.log(msg);
 };
 
 module.exports = displayMessage;
-
