2058543567bbfb53b63693c1826e767e172ca608
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2025-02-06 06:08:55 +0100

    added a function that print to STDOUT

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * A function that display a message on STDOUT
+ * @params {string) message  The message to be displayed
+ */
+
+const displayMessage = (message) => {
+	console.log(message);
+};
+
+module.exports = displayMessage
